cd68382796569be2a1fcf9c5243bc8831ad176de
viet34tqc <viet34tqc@gmail.com>
2022-05-03 17:10:13 +0700

    If logged out user, go to dashboard redirect to login page

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { useAuth } from '@/context/AuthContext';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
+	const navigate = useNavigate();
+	const { user } = useAuth();
+
+	useEffect(() => {
+		if (!user) {
+			navigate('/auth/login');
+		}
+	}, [user]);
 	return (
 		<div>
 			<Outlet />
