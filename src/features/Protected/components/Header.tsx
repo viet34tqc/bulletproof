b5cb366144acd5fe9e228ff71c7f42111121326e
viet34tqc <viet34tqc@gmail.com>
2022-05-06 09:18:11 +0700

    UserNavigation

--- src/features/Protected/components/Header.tsx
+++ src/features/Protected/components/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import UserNavigation from './UserNavigation';
 
 const Header = () => {
-	return <div className="relative z-10 bg-white shadow p-6">Header</div>;
+	return (
+		<div className="relative z-10 bg-white shadow p-6 flex justify-between md:justify-end">
+			<UserNavigation />
+		</div>
+	);
 };
 
 export default Header;
